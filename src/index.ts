61c841d3ee1fdceb156a5fc22950af8addf0710a
Guilherme Rodrigues <guilherme.imlerith@gmail.com>
2021-03-21 14:58:03 -0300

    Add Swagger docs

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import express from "express";
+import swaggerUI from "swagger-ui-express";
 
 import { usersRoutes } from "./routes/users.routes";
+import swaggerConfig from "./swaggerConfig.json";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerConfig));
 
 app.use("/users", usersRoutes);
 
